76b66dfc69a415910aec2e6f7220d2ffde20a7c3
RyanBln <ryanvikaiz@gmail.com>
2020-11-25 14:54:50 +0100

    commit9

--- joueur.c
+++ joueur.c
@@ -4,8 +4,15 @@
 
 /* Ryan */
 
-typedef struct joueur {
-    char *nom; 
-    char *prenom;
-} Joueur; 
+typedef struct {
+    char *pseudo; 
+} joueur; 
+
+void pseudo() {
+    char pseudo[10]; 
+    printf("Quel est votre pseudo ?\n"); 
+    scanf("%s", pseudo); 
+    printf ("Vous jouez sous le pseudo de : %s ! \n \n ", pseudo); 
+}
+
 
