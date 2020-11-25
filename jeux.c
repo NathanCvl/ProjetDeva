3071ab8f54543794bb4626e7f3d74daa577f537e
Nathan <nathancuvelier59@icloud.com>
2020-11-25 20:59:56 +0100

    commit 10

--- jeux.c
+++ jeux.c
@@ -25,12 +25,13 @@ void afficher_jeux(jeux p){
         }
     }
 }
-Bateau type_bateau(jeux p){
+void type_bateau(jeux p){
     p.porte_avion.taille=Bat_porte_avion;
     p.croiseur.taille = Bat_croiseur;
     p.contre_torpilleur.taille = Bat_contre_torpilleur;
     p.sous_marin.taille = Bat_sous_marin;
     p.torpilleur.taille = Bat_torpilleur;
+    
 }
 
 int main(int argc, char const *argv[])
--- joueur.c
+++ joueur.c
@@ -1,4 +1,4 @@
-#include <joueur.h> 
+#include "joueur.h"
 #include <stdio.h>
 #include <stdlib.h>
 
